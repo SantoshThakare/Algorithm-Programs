097beb38bfebbea1d768ea1fde6e520bfe26c976
SantoshThakare <santoshthakare50@gmail.com>
2022-04-27 19:12:35 +0530

    Add Algorithm Program

--- anagram.py
+++ anagram.py
@@ -1,6 +1,5 @@
 
-_str1 = input("Enter the First String : ")
-_str2 = input("Enter the Second String: ")
+
 
 def _anagram():
     if sorted(_str1) == sorted(_str2):
@@ -10,5 +9,8 @@ def _anagram():
         return "string are not Anagram"
 
 if __name__ == '__main__':
+
+    _str1 = input("Enter the First String : ")
+    _str2 = input("Enter the Second String: ")
     result = _anagram()
     print(result)
---
+++ permutation.py
@@ -0,0 +1,8 @@
+import itertools
+
+
+if __name__ == '__main__':
+    s = 'ABC'
+    nums = list(s)
+    permutations = list(itertools.permutations(nums))
+    print([''.join(permutation) for permutation in permutations])
